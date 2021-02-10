2f9f7fe0f23a1cb400975b6e12274246394c6506
ashutosh <choudhayashutosh7070@gmail.com>
2021-02-11 04:03:43 +0530

    attached linkedin and github and email

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,9 @@
     let skills=document.querySelector("#skills");
     let projects=document.querySelector("#projects");
     let contact=document.querySelector("#contact");
+    let sendButton=document.querySelector("#send");
+    let linkedin=document.querySelector("#linkedin");
+    let github=document.querySelector("#github");
     home.addEventListener('click',()=>{
         scrollElement.scrollTop=0;
     });
@@ -23,5 +26,21 @@
     });
     contact.addEventListener('click',()=>{
         scrollElement.scrollTop=document.querySelector("#contact-heading").offsetTop;
+    });
+    sendButton.addEventListener('click',()=>{
+        let name=document.querySelector("#name");
+        let subject=document.querySelector("#subject");
+        let body=document.querySelector("#body");
+        if(name.value=="" || subject.value=="" || body.value==""){
+            {alert("Please fill all boxes.");return;}
+        
+        }
+        window.open("mailto:choudharyashutosh7070@gmail.com?subject="+subject.value+"&body="+name.value+":"+body.value,"_black");
+    });
+    linkedin.addEventListener("click",()=>{
+        window.open("https://www.linkedin.com/in/ashutosh-choudhary-73baa1189","_blank");
+    })
+    github.addEventListener("click",()=>{
+        window.open("https://github.com/ChoudharyAshutosh","_blank");
     });
 })();
